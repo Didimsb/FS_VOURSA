5d0b7908a27a3ca15d4309bd484f10931d849c19
ahmedEssyad <essyadmo1989@gmail.com>
2025-04-23 22:35:15 +0000

    Update AdminService.js

--- voursa/src/services/AdminService.js
+++ voursa/src/services/AdminService.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
 
-// Create axios instance with default config
+
 const axiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json'
   }
